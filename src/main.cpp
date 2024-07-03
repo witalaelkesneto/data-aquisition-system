6317b5ab3f6cc38e387488c04b3fa3bcd132ef64
witalaelkesneto <100223334+witalaelkesneto@users.noreply.github.com>
2024-07-03 02:29:06 +0000

    Finalizando o exercício computacional

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include <vector>
 #include <ctime>
 #include <iomanip>
+#include <fstream>
+#include <filesystem>
 
 using boost::asio::ip::tcp;
 
@@ -28,6 +30,13 @@ std::time_t string_to_time_t(const std::string &time_string)
     return std::mktime(&tm);
 }
 
+std::string time_t_to_string(std::time_t time) {
+    std::tm* tm = std::localtime(&time);
+    std::ostringstream ss;
+    ss << std::put_time(tm, "%Y-%m-%dT%H:%M:%S");
+    return ss.str();
+}
+
 class session
     : public std::enable_shared_from_this<session>
 {
@@ -46,38 +55,84 @@ private:
     void read_message()
     {
         auto self(shared_from_this());
-        boost::asio::async_read_until(socket_, buffer_, "\r\n",
-                                      [this, self](boost::system::error_code ec, std::size_t length)
+        boost::asio::async_read_until(socket_, buffer_, "\r\n", [this, self](boost::system::error_code ec, std::size_t length)
                                       {
-                                          if (!ec)
-                                          {
-                                              std::istream is(&buffer_);
-                                              std::string message(std::istreambuf_iterator<char>(is), {});
-                                              std::cout << "Received: " << message << std::endl;
+            if (!ec) {
+            std::istream is(&buffer_);
+            std::string message(std::istreambuf_iterator<char>(is), {});
+            std::cout << "Received: " << message << std::endl;
 
-                                              // Usar std::istringstream para facilitar o parse
-                                              std::istringstream iss(message);
-                                              std::string token;
-                                              std::vector<std::string> tokens;
+            // Usar std::istringstream para facilitar o parse
+            std::istringstream iss(message);
+            std::string token;
+            std::vector<std::string> tokens;
 
-                                              while (std::getline(iss, token, '|'))
-                                              {
-                                                  tokens.push_back(token);
-                                              }
+            while (std::getline(iss, token, '|')) {
+                tokens.push_back(token);
+            }
 
-                                              LogRecord log;
+            if (tokens[0] == "LOG") {
+                LogRecord log;
 
-                                              strcpy(log.sensor_id, tokens[1].c_str());
-                                              log.timestamp = string_to_time_t(tokens[2]);
-                                              log.value = std::stod(tokens[3]);
+                strcpy(log.sensor_id, tokens[1].c_str());
+                log.timestamp = string_to_time_t(tokens[2]);
+                log.value = std::stod(tokens[3]);
 
-                                              std::cout << log.sensor_id << std::endl;
-                                              std::cout << log.timestamp << std::endl;
-                                              std::cout << log.value << std::endl;
+                std::string filePath = "../logs/";
+                std::string fileName = std::string(log.sensor_id) + ".dat";
 
-                                              write_message(message);
-                                          }
-                                      });
+                std::fstream file(filePath + fileName , std::fstream::out | std::fstream::binary | std::fstream::app);
+
+                if (!file.is_open()) {
+                    std::cout << "Falha ao criar o arquivo" << std::endl;
+                }
+
+                file.write((char*)&log, sizeof(LogRecord));
+                file.close();
+            }else if(tokens[0] == "GET"){
+
+                int n = std::stoi(tokens[2]);
+
+                std::string filePath = "../logs/";
+                std::string fileName = std::string(tokens[1].c_str()) + ".dat";
+
+                if(std::filesystem::exists(filePath + fileName)){
+                    std::fstream file(filePath + fileName , std::fstream::in | std::fstream::binary);
+
+                    if (!file.is_open()) {
+                        std::cout << "Falha ao abrir o arquivo" << std::endl;
+                    }
+
+                    file.seekg(0, std::ios_base::end);
+
+                    int file_size = file.tellg();
+                    int numRecords = file_size/sizeof(LogRecord);
+                    int startPos = file_size - n * sizeof(LogRecord);
+
+                    if (startPos < 0) {
+                        startPos = 0;
+                        n = numRecords;
+                    }
+
+                    file.seekg(startPos, std::ios_base::beg);
+
+                    LogRecord log;
+
+                    message = std::to_string(n) + ";";
+
+                    while (file.read((char*)&log, sizeof(LogRecord))) {
+                        message = message + time_t_to_string(log.timestamp) + "|" + std::to_string(log.value) + ";";
+                    }
+
+                    message.append("\r\n");
+
+                    file.close();
+                }else{
+                    message = "ERROR|INVALID_SENSOR_ID\r\n";
+                }
+            }
+                write_message(message);
+            } });
     }
 
     void write_message(const std::string &message)
